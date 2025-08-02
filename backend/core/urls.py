94363fad84899af54933f39d9e0f8090e2cbfb3f
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-08-02 12:12:29 +0500

    backend-refactor: configured settings.py and urls.py files

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,6 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posapi.urls')),
-    # You can also add API documentation URLs here later
-    # path('api/docs/', include('rest_framework.urls')),
 ]
