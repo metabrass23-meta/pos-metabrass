ec073e2d2c1155784141e4ac63499308c391d2d2
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-08-02 18:57:04 +0500

    backend-refactor: configured project's urls.py and make migrations

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posapi.urls')),
     path('api/v1/categories/', include('categories.urls')),
+    path('api/v1/products/', include('products.urls')),
 ]
