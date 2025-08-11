91664209ca3a458d805a317be05df4e6ece47937
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-08-11 20:31:34 +0500

    backend-feat: created labors app, configured project's settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'products',
     'customers',
     'vendors',
-    'orders',
+    'labors',
 ]
 
 MIDDLEWARE = [
--- backend/labors/apps.py
+++ backend/labors/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OrdersConfig(AppConfig):
+class LaborsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orders'
+    name = 'labors'
