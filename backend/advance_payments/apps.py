7744b36df8d057df6d7f4da78afac159f9aade1d
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-08-13 17:12:46 +0500

    backend-feat: created advance payments app and modified settings.py

--- backend/advance_payments/apps.py
+++ backend/advance_payments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdvancePaymentConfig(AppConfig):
+class AdvancePaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'advance_payment'
+    name = 'advance_payments'
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,10 @@ urlpatterns = [
     path('api/v1/customers/', include('customers.urls')),
     path('api/v1/vendors/', include('vendors.urls')),
     path('api/v1/labors/', include('labors.urls')),
-]   
+    path('api/v1/advance-payments/', include('advance_payments.urls')),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
