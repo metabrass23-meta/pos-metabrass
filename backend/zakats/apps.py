373cbc9e9187c2f299049894f6da38057da7770d
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-08-15 01:40:02 +0500

    backend-feat: added views for zakat module, modified admin.py file for admin panel and configured apps.py file

--- backend/zakats/apps.py
+++ backend/zakats/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
 
-class ZakatsConfig(AppConfig):
+class ZakatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'zakats'
+    verbose_name = 'Zakat Management'
+    
+    def ready(self):
+        """
+        Import signals when the app is ready
+        """
+        import zakats.signals
