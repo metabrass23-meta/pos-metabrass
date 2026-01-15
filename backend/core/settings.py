b4e324c4f0b12c6ff1fce5514173fc1127c8e3b7
abdullahKhanKhetran <abdullahkhitran2005@gmail.com>
2026-01-15 20:58:36 +0500

    "Update settings for Railway deployment"

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -156,6 +156,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.pagination.PageNumberPagination'
     ],
     'PAGE_SIZE': 20,
 }
